7be05ac8e30141d9cf2b43e64ca0dc27bafdee65
HCTMZ <132988066+HCTMZ@users.noreply.github.com>
2023-08-02 09:30:05 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		//VendingMachineView vendView = new VendingMachineView();
+		VendingMachineController vendController = new VendingMachineController();
+	}
+}
